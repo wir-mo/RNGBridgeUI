eae6ac3f1d7d99ef70cc091a00ecd2c43e06d153
Moritz Wirger <lj.moritorii@web.de>
2022-07-11 17:32:02 +0200

    Bump version to 2.4.0

--- src/esp-api-mock.js
+++ src/esp-api-mock.js
@@ -6,11 +6,11 @@ let status_data = {
 	uptime: 1200,
 	// MQTT status
 	mqtt: {
-		status: "connected", //Can be either disabled or connected or disconnected
+		status: 'connected', //Can be either disabled or connected or disconnected
 	},
 	// PVOutput status
 	pvo: {
-		status: "disabled", //Can be either disabled or connected or disconnected
+		status: 'disabled', //Can be either disabled or connected or disconnected
 	},
 	// System status
 	loadEnabledTemp: false,
@@ -41,50 +41,50 @@ let status_data = {
 	},
 	network: {
 		wifi_client: {
-			status: "disabled", //Can be either disabled or connected or disconnected
-			ip: "111.222.333.444",
-			netmask: "255.255.255.0",
-			dns: "1.3.4.5"
+			status: 'disabled', //Can be either disabled or connected or disconnected
+			ip: '111.222.333.444',
+			netmask: '255.255.255.0',
+			dns: '1.3.4.5'
 		},
 		wifi_ap: {
-			status: "disabled", //Can be either disabled or enabled
-			ip: "111.222.333.444",
+			status: 'disabled', //Can be either disabled or enabled
+			ip: '111.222.333.444',
 		},
 	},
 }
 
 let config_data = {
 	dev: {
-		name: "CustomName",
+		name: 'CustomName',
 	},
 	wifi: {
 		client_enabled: false,
 		client_dhcp_enabled: true,
-		client_ssid: "YourWifi-AP",
+		client_ssid: 'YourWifi-AP',
 		client_has_password: false,
 		client_password: null,
-		client_ip: "192.168.0.200",
-		client_mask: "255.255.255.0",
-		client_gateway: "192.168.0.1",
-		client_dns: "192.168.0.1",
+		client_ip: '192.168.0.200',
+		client_mask: '255.255.255.0',
+		client_gateway: '192.168.0.1',
+		client_dns: '192.168.0.1',
 		ap_enabled: true,
-		ap_ssid: "RNGBridge",
+		ap_ssid: 'RNGBridge',
 		ap_has_password: false,
-		ap_password: "",
+		ap_password: '',
 	},
 	mqtt: {
 		enabled: false,
-		server: "192.168.1.74",
+		server: '192.168.1.74',
 		port: 1883,
-		id: "RNGBridge",
-		user: "xxx",
+		id: 'RNGBridge',
+		user: 'xxx',
 		has_password: false,
 		password: null,
-		topic: "/rng",
+		topic: '/rng',
 	},
 	pvo: {
 		enabled: false,
-		api_key: "",
+		api_key: '',
 		system_id: 0,
 		time_offset: 0,
 	},
@@ -95,38 +95,48 @@ let config_data = {
 export function makeServer() {
 	createServer({
 		routes() {
-			this.post("/api/control", (schema, request) => {
+			this.post('/api/control', (schema, request) => {
 				let json = JSON.parse(request.requestBody)
-				const loadEnabled = json["enabled"];
+				const loadEnabled = json['enabled'];
 				status_data.l.enabled = loadEnabled;
-				return "OK";
+				return 'OK';
 			});
 
-			this.post("/api/config", (schema, request) => {
+			this.post('/api/config', (schema, request) => {
 				let json = JSON.parse(request.requestBody)
-				const wifiConfig = json["wifi"];
+				const wifiConfig = json['wifi'];
 				if (wifiConfig != null) {
-					config_data["wifi"] = wifiConfig;
+					config_data['wifi'] = wifiConfig;
 				}
-				const mqttConfig = json["mqtt"];
+				const mqttConfig = json['mqtt'];
 				if (mqttConfig != null) {
-					config_data["mqtt"] = mqttConfig;
+					config_data['mqtt'] = mqttConfig;
 				}
-				const pvoConfig = json["pvo"];
+				const pvoConfig = json['pvo'];
 				if (pvoConfig != null) {
-					config_data["pvo"] = pvoConfig;
+					config_data['pvo'] = pvoConfig;
 				}
-				return "OK";
+				return 'OK';
 			});
 
-			this.get("/api/config", () => {
+			this.get('/api/config', () => {
 				return config_data;
 			});
 
-			// this.get("/api/status", () => {
+			// this.get('/api/status', () => {
 			// 	status_data.uptime = (Date.now() - startTime) / 1000
 			// 	return status_data
 			// })
+
+			// this.get('/events', () => {
+			// 	// res.status(200).set();
+			// 	// res.write(`data: Hello there \n\n`);
+			// 	console.log('/events');
+			// 	// return { 'connection': 'keep-alive', 'content-type': 'text/event-stream' };
+			// 	let resp = new Response();
+			// 	resp.headers.append
+			// 	return new Response(200, { 'connection': 'keep-alive', 'content-type': 'text/event-stream' }, `event: status\r\ndata: ${status_data}`);
+			// });
 
 			this.passthrough();
 			this.passthrough('https://raw.githubusercontent.com/**');
