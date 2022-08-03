3eeeaa5fd9fe1e52e018639cebf8434162384a53
Moritz Wirger <lj.moritorii@web.de>
2022-08-03 20:28:00 +0200

    Bump version to 2.7.1

--- src/App.vue
+++ src/App.vue
@@ -631,7 +631,7 @@
                     <v-col cols="8" class="text--secondary">
                       <v-fade-transition leave-absolute>
                         <v-row v-if="!open" no-gutters style="width: 100%">
-                          <v-col cols="6">Software Version: 2.7.0</v-col>
+                          <v-col cols="6">Software Version: 2.7.1</v-col>
                         </v-row>
                       </v-fade-transition>
                     </v-col>
