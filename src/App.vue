4f8dcff6bfdcb6f49e8bc9d7ce93218898fa81a3
Moritz Wirger <lj.moritorii@web.de>
2022-08-04 23:42:21 +0200

    Bump version to 2.7.3
    - Bump dependencies

--- src/App.vue
+++ src/App.vue
@@ -637,7 +637,7 @@
                     <v-col cols="8" class="text--secondary">
                       <v-fade-transition leave-absolute>
                         <v-row v-if="!open" no-gutters style="width: 100%">
-                          <v-col cols="6">Software Version: 2.7.2</v-col>
+                          <v-col cols="6">Software Version: 2.7.3</v-col>
                         </v-row>
                       </v-fade-transition>
                     </v-col>
--- src/esp-api-mock.js
+++ src/esp-api-mock.js
@@ -12,7 +12,7 @@ let status_data = {
 	pvo: {
 		status: 'disabled', //Can be either disabled or connected or disconnected
 	},
-	ota: "2.7.2",
+	ota: "2.7.3",
 	// System status
 	loadEnabledTemp: false,
 	b:
