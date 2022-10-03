deffb75eec5aaa41d655aeeab6fb8b01f659eeb0
Moritz Wirger <lj.moritorii@web.de>
2022-10-03 10:46:20 +0200

    Bump version to 2.9.1
    - Bump dependencies

--- src/esp-api-mock.js
+++ src/esp-api-mock.js
@@ -12,7 +12,7 @@ let status_data = {
 	pvo: {
 		status: 'disabled', //Can be either disabled or connected or disconnected
 	},
-	ota: "2.9.0",
+	ota: "2.9.1",
 	// System status
 	loadEnabledTemp: false,
 	b:
