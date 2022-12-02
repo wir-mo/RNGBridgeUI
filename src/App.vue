be6144971f14fcc16f290b65eea214aecea20ad8
Moritz Wirger <lj.moritorii@web.de>
2022-12-02 20:30:32 +0100

    Bump version to 2.11.0
    - Bump dependencies

--- src/App.vue
+++ src/App.vue
@@ -650,7 +650,7 @@
                     <v-col cols="8" class="text--secondary">
                       <v-fade-transition leave-absolute>
                         <v-row v-if="!open" no-gutters style="width: 100%">
-                          <v-col cols="6">Software Version: 2.10.0</v-col>
+                          <v-col cols="6">Software Version: 2.11.0</v-col>
                         </v-row>
                       </v-fade-transition>
                     </v-col>
